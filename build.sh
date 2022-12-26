f23d90825f2d0640d0c290b9c697413fededc00b
Sergei Doroshenko <sddorosh@amazon.com>
2022-12-26 11:24:11 +0100

    Add config

--- build.sh
+++ build.sh
@@ -2,15 +2,51 @@
 
 set -euo pipefail
 
+CDK_PROJECT_PATH=""
+LAMBDAS_PROJECT_PATH=""
+
+config() {
+  echo "Checking config file local.properties"
+  if [ -e local.properties ]
+  then
+      echo "Configuration exists"
+      cat local.properties
+  else
+      echo "Configuration not found. Creating configuration file: local.properties"
+      echo "Set CDK_PROJECT_PATH:"
+      read -r CDK_PROJECT_PATH
+
+      echo "Set LAMBDAS_PROJECT_PATH:"
+      read -r LAMBDAS_PROJECT_PATH
+      printf "path.cdk=%s\n" "$CDK_PROJECT_PATH" >> local.properties
+      printf "path.lambdas=%s\n" "$LAMBDAS_PROJECT_PATH" >> local.properties
+  fi
+}
+
+change-dir() {
+  local paths
+  paths=(cdk lambdas)
+  if [[ ! " ${paths[*]} " =~ $1 ]]; then
+      echo "Wrong value $1"
+      exit 1
+  fi
+
+  echo "Changing to $1"
+  local path
+  path=$(sed -n "s/^path.$1=\(.*\)/\1/p" local.properties)
+
+  cd "$path" || exit
+}
+
 build-kotlin() {
   echo "Building Kotlin lambdas..."
-  cd lambdas || exit
+  change-dir lambdas
   gradle clean makeDeploymentBundle
   cd ..
 }
 
 build-stacks() {
-  cd cdk
+  change-dir cdk
   echo "Removing previous artifacts"
   rm -rf ./cdk.out/*.zip || true
   rm -rf ./cdk.out/*.jar || true
@@ -25,20 +61,20 @@ build-all() {
 }
 
 invoke() {
-  cd cdk
+  change-dir cdk
   sam local invoke CdkSamInputFunction -e events/apiProxy.json
 #  sam local invoke CdkSamComputeFunction -e events/receiveMessage.json
   cd ..
 }
 
 logs() {
-  cd cdk
+  change-dir cdk
   sam logs -n CdkSamInputFunction --stack-name CdkStack -t
   cd ..
 }
 
 start-api() {
-  cd cdk
+  change-dir cdk
   sam local start-api --warm-containers EAGER
   cd ..
 }
@@ -51,7 +87,7 @@ test-api() {
 
 deploy() {
   echo "Deploying application..."
-  cd ./cdk
+  change-dir cdk
   cdk bootstrap
   local output
   output=$(cdk deploy --require-approval never --all)
@@ -61,7 +97,7 @@ deploy() {
 
 destroy() {
   echo "Destroying application..."
-  cd ./cdk
+  change-dir cdk
   cdk destroy --all --require-approval never
   cd ..
 }
